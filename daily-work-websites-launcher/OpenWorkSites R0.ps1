374ea09b2167cf15bcb38df5c3efe88d75b0f3b7
Nima Geran Malek <70598630+nimageran@users.noreply.github.com>
2025-08-01 11:22:59 -0500

    Create OpenWorkSites R0.ps1

---
+++ daily-work-websites-launcher/OpenWorkSites R0.ps1
@@ -0,0 +1,6 @@
+# Open three work websites in separate Edge windows
+Start-Process msedge "https://viewer.autodesk.com/designviews"
+Start-Sleep -Seconds 1
+Start-Process msedge "https://mtm115.sharepoint.com/sites/Quality/Lists/ARDescription/AllItems.aspx"
+Start-Sleep -Seconds 1
+Start-Process msedge "https://chatgpt.com/g/g-p-67a0fc3e1c5c8191abd8f5feee50e23a-mywork-mtm/project"
